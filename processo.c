e24666811700cbdd6f747d62a5c4b3468d5c96ae
Liliswep <liliam.silvateixeira@gmail.com>
2025-04-09 15:24:06 -0300

    Dados tratados corretamente

--- processo.c
+++ processo.c
@@ -1,8 +1,74 @@
 #include "processo.h"
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LINHAS 15
+#include <stdio.h>
 
+#define MAX_LINHAS 18393
+#define MAX_CAMPOS 10
+#define TAM_CAMPO 256
+
+void limparMultivalorado(char *campo) {
+    char temp[100];
+    int j = 0;
+
+    for (int i = 0; campo[i] != '\0'; i++) {
+        char c = campo[i];
+        if (c != '{' && c != '}' && c != '"' && c != ',') {
+            temp[j++] = c;
+        }
+    }
+
+    temp[j] = '\0';
+    strcpy(campo, temp);
+}
+//vai ler o arquivo sem as ""
+//vai retornar o index da coluna tal tratada
+//Parsing: solução encontrada lendo em manual C e foruns stackoverflow para tratamento de linhas de string complexas
+int parseCSV(char *linha, char campos[][TAM_CAMPO], int max_campos) {
+    int index = 0;
+    char *cursor = linha;
+
+    while (*cursor && index < max_campos) {
+        //percorre o indice, a coluna e cada caracter dela com o cursosr
+        if (*cursor == '"') {
+            cursor++;// se o cursor encontrar as aspas ele pula as aspas
+            char *inicioCampo = cursor; //inicio de campo agr tem os valores entre as aspas
+            
+            int tamanho = 0; 
+            //procura a proxima aspas para pular
+            while (*cursor && *cursor != '"') {
+                cursor++;
+                tamanho++;// contador de caracteres
+            }
+            // copia inicio de campo para campos index, e o numero de caracteres a serem copiados
+            strncpy(campos[index], inicioCampo, tamanho);
+            campos[index][tamanho] = '\0';
+
+            //Após terminar o campo, se o cursor está numa aspa ("), pula essa aspa de fechamento.
+            if (*cursor == '"') cursor++;
+            if (*cursor == ',') cursor++;
+        } else {
+            char *inicioCampo = cursor;
+            int tamanho = 0;
+
+            while (*cursor && *cursor != ',') {
+                cursor++;
+                tamanho++;
+            }
+
+            strncpy(campos[index], inicioCampo, tamanho);
+            campos[index][tamanho] = '\0';
+
+            if (*cursor == ',') cursor++;
+        }
+
+        index++;
+    }
+
+    return index;
+}
+
+//função para ler o CSV
 Processo *LerDados(const char *nomeArquivo, int *n) {
     FILE *fp = fopen(nomeArquivo, "r");
     if (fp == NULL) {
@@ -10,50 +76,47 @@ Processo *LerDados(const char *nomeArquivo, int *n) {
         exit(1);
     }
 
-    Processo *X = malloc(MAX_LINHAS * sizeof(Processo));
-    if (X == NULL) {
+    Processo *processo = malloc(MAX_LINHAS * sizeof(Processo));
+    if (processo == NULL) {
         printf("ERRO: Não foi possível alocar memória para a tabela!\n");
         exit(1);
     }
 
-    char Cabecalho[100];
-    fscanf(fp, "%s", Cabecalho);
-    *n = 0;
+    char linha[512];
+    int i = 0;
 
-    char linha[512]; 
+    if (fgets(linha, sizeof(linha), fp) == NULL) {
+        fclose(fp);
+        free(processo);
+        return NULL;
+    }
 
-    while (*n < MAX_LINHAS && fgets(linha, sizeof(linha), fp)) {
-        if (sscanf(linha, "%d,\"%99[^\"]\",%99[^,],%99[^,],%99[^,],%d",
-                   &X[*n].id, X[*n].numProcesso,
-                   X[*n].data_ajuizamento, X[*n].id_classe,
-                   X[*n].id_assunto, &X[*n].ano_eleicao) == 6) {
+    while (fgets(linha, sizeof(linha), fp) && i < MAX_LINHAS) {
+        linha[strcspn(linha, "\n")] = '\0';
 
-            int i = 0, j = 0;
-            while (X[*n].id_classe[i] != '\0') {
-                if (X[*n].id_classe[i] != '{' && X[*n].id_classe[i] != '}' && X[*n].id_classe[i] != '"') {
-                    X[*n].id_classe[j++] = X[*n].id_classe[i];
-                }
-                i++;
-            }
-            X[*n].id_classe[j] = '\0';
+        char campos[6][256] = {{0}};
+        int qtd = parseCSV(linha, campos, 6);
+        // depois de ler os valores sem aspas ele vai devolvolver e copiar no struct
+        if (qtd == 6) {
+            processo[i].id = atoi(campos[0]);
+            strncpy(processo[i].numProcesso, campos[1], sizeof(processo[i].numProcesso));
+            strncpy(processo[i].data_ajuizamento, campos[2], sizeof(processo[i].data_ajuizamento));
+            strncpy(processo[i].id_classe, campos[3], sizeof(processo[i].id_classe));
+            strncpy(processo[i].id_assunto, campos[4], sizeof(processo[i].id_assunto));
+            processo[i].ano_eleicao = atoi(campos[5]);
 
-            i = j = 0;
-            while (X[*n].id_assunto[i] != '\0') {
-                if (X[*n].id_assunto[i] != '{' && X[*n].id_assunto[i] != '}' && X[*n].id_assunto[i] != '"') {
-                    X[*n].id_assunto[j++] = X[*n].id_assunto[i];
-                }
-                i++;
-            }
-            X[*n].id_assunto[j] = '\0';
+            limparMultivalorado(processo[i].id_classe);
+            limparMultivalorado(processo[i].id_assunto);
 
-            (*n)++;
+            i++;
         }
     }
 
     fclose(fp);
-    return X;
+    *n = i;
+    return processo;
 }
-
+//1. Ordenar, em ordem crescente, o conjunto de dados a partir do atributo “id”;
 void swap(Processo *a, Processo *b) {
     Processo tmp = *a;
     *a = *b;
@@ -84,6 +147,7 @@ void ordenarPorId(Processo *vet, int n) {
     quicksort(vet, 0, n - 1, compararPorId);
 }
 
+//Salvar em csv 1 e 2
 void salvarCSV(const char *nomeArquivo, Processo *vet, int n) {
     FILE *fp = fopen(nomeArquivo, "w");
     if (!fp) {
@@ -105,4 +169,34 @@ void salvarCSV(const char *nomeArquivo, Processo *vet, int n) {
 
     fclose(fp);
     printf("Dados salvos com sucesso em %s\n", nomeArquivo);
+}
+
+//3. Contar quantos processos estão vinculados a um determinado “id_classe”;
+void ContarPorClasse(Processo processos[], int n) {
+    char idsAgrupados[5000][50]; // Vetor local de id_classe únicos
+    int contadores[5000] = {0};  // Contador de ocorrências
+    int totalGrupos = 0;
+
+    for (int i = 0; i < n; i++) {
+        int encontrado = 0;
+
+        for (int j = 0; j < totalGrupos; j++) {
+            if (strcmp(processos[i].id_classe, idsAgrupados[j]) == 0) {
+                contadores[j]++;
+                encontrado = 1;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            strcpy(idsAgrupados[totalGrupos], processos[i].id_classe);
+            contadores[totalGrupos] = 1;
+            totalGrupos++;
+        }
+    }
+
+    printf("\nContagem de processos por id_classe:\n");
+    for (int i = 0; i < totalGrupos; i++) {
+        printf("id_classe: %s tem: %d processos\n", idsAgrupados[i], contadores[i]);
+    }
 }
